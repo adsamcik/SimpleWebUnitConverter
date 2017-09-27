0bb107aaa12d04864d56840790ebba64966f8ca1
adsamcik <adsamcik@gmail.com>
2017-09-27 15:32:25 +0200

    Add conversion to km, m, cm, mm

--- matcher.js
+++ matcher.js
@@ -1,10 +1,40 @@
-function replaceInches(match, offset, string) {
-    console.log("MATCH " + match);
+function readableMilimeters(mm) {
+    function print(mm, cm, m, km) {
+        var string = "";
+        if (km != 0)
+            string += km + " km ";
+        if (m != 0)
+            string += m + " m ";
+        if (cm != 0)
+            string += cm + " cm ";
+        if (mm != 0)
+            string += mm + " mm ";
+        return string.trim();
+    }
 
+    if (mm > 10) {
+        var cm = Math.floor(mm / 10);
+        mm -= cm * 10;
+        if (cm > 100) {
+            var m = Math.floor(cm / 100);
+            cm -= m * 100;
+            if (m > 1000) {
+                var km = Math.floor(m / 1000);
+                m -= km * 1000;
+                return print(mm, cm, m, km);
+            }
+            return print(mm, cm, m, 0);
+        }
+        return print(mm, cm, 0, 0);
+    }
+    return print(mm, 0, 0, 0);
+}
+
+function replaceInches(match, offset, string) {
     var footEnd = match.indexOf("'");
     var mm = parseInt(match.substring(0, match.indexOf("′"))) * 304.8;
     mm += parseInt(match.substring(footEnd, match.indexOf("″"))) * 25.4;
-    return mm + " mm";
+    return readableMilimeters(mm) + " (" + match + ")";
 }
 
 
